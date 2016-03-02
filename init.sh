428f9d02b99be1d6456ae5c8d8ac75b0f55d7a37
prioz <k@prioz.ru>
2016-03-03 00:48:36 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/test
 sudo /etc/init.d/nginx restart
 gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
