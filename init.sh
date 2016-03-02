a1c6643fc3d091a4250289b5ecf12f9a533d4477
prioz <k@prioz.ru>
2016-03-03 01:12:56 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
 gunicorn -D -c /etc/gunicorn.d/hello.py hello:wsgi_application
+#sudo nano /var/log/nginx/error.log
