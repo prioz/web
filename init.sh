8167c0ad49ed883aaad7ebb5f536494e47251e74
prioz <k@prioz.ru>
2016-03-03 01:22:21 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
