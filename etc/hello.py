7763a43779953c53ad5d06b95e197ba8cbabb9ff
prioz <k@prioz.ru>
2016-03-03 11:47:42 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,4 +1,7 @@
 bind = "0.0.0.0:8080"
 logfile = "/home/box/g.log"
-loglevel = "info"
+loglevel = "debug"
+errorlog = '/home/box/web/g-e.log'
+accesslog = '/home/box/web/g-a.log'
+loglevel = 'debug'
 pythonpath= "/home/box/web"
