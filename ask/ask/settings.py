edfa328729b234e89c6a4437fecaf78bfa8572f1
prioz <k@prioz.ru>
2016-03-03 19:09:46 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'box_django',
+        'USER': 'box',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
