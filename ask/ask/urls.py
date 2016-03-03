cf8db6226d300f3fa7f665b2cf2bbf8823a0ea61
prioz <k@prioz.ru>
2016-03-03 12:43:22 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -8,5 +8,13 @@ urlpatterns = patterns('',
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'ask.qa.views.test', name='test'),
+    url(r'^login/', 'ask.qa.views.test', name='test'),
+    url(r'^signup/', 'ask.qa.views.test', name='test'),
+    url(r'^question/[0-9]*?/', 'ask.qa.views.test', name='test'),
+    url(r'^ask/', 'ask.qa.views.test', name='test'),
+    url(r'^popular/', 'ask.qa.views.test', name='test'),
+    url(r'^new/', 'ask.qa.views.test', name='test'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
