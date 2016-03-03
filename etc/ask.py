9af528dbdff003c16838f2a938848dde90bf4092
prioz <k@prioz.ru>
2016-03-03 12:02:55 +0300

    Create ask.py

---
+++ etc/ask.py
@@ -0,0 +1,8 @@
+
+bind = "0.0.0.0:8000"
+logfile = "/home/box/g.log"
+loglevel = "debug"
+errorlog = '/home/box/web/g-e.log'
+accesslog = '/home/box/web/g-a.log'
+loglevel = 'debug'
+pythonpath= "/home/box/web"
